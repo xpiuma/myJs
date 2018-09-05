7a895cca808aa9914659d7eed942c6f24773abc6
Davide <davide.piumetti@accenture.com>
2018-09-05 15:40:51 +0200

    First

---
+++ index.js
@@ -0,0 +1,27 @@
+  // Comment
+  let person = {
+    name: 'Davide',
+    age: 36
+  }
+  
+   sadasdasd\1111111
+   
+
+  function greet(name, lastName) {
+    console.log('Hello ' + name + ' ' + lastName);
+  }
+
+
+  greet('Davide', 'Piumetti');
+
+  greet('Mondo');
+
+
+
+  function square(number) {
+    return number*number;
+  }  
+
+  let number = square(2);
+  console.log(number);
+  console.log(square(number));
