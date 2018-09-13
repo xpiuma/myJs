2eb856f0296f36eca4287a41b1b5ab119dba1732
Davide <davide.piumetti@accenture.com>
2018-09-13 11:12:14 +0200

    f

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
     age: 36
   }
   
-   sadasdasd\1111111
    
 
   function greet(name, lastName) {
@@ -25,3 +24,62 @@
   let number = square(2);
   console.log(number);
   console.log(square(number));
+
+  console.log(1 === 1);
+  console.log('1' === 1);
+  console.log(1 == 1);
+  console.log('1' == 1);
+  console.log(true == 0);
+  console.log(true == 1);
+
+  let points = 110;
+  let type = points > 100 ? 'gold' : 'silver';
+  console.log(type);
+
+
+  let highIncome = true;
+  let goodCreditScore = true;
+  let eligibleForLoan = highIncome && goodCreditScore;
+  let applicationRefused = !eligibleForLoan;
+  console.log('Eligible', eligibleForLoan);
+  console.log('Application refused', applicationRefused);
+
+  let userColor = undefined;
+  let defaultColor = 'blue';
+  let currentColor = userColor || defaultColor;
+  console.log(currentColor);
+
+
+  const readPermission = 4;
+  const writePermission = 2;
+  const executePermission = 1;
+
+  let myPermission = 0;
+  myPermission = myPermission | writePermission | readPermission;
+
+  let message = (myPermission & readPermission) ? 'Yes' : 'No';
+  console.log(message);
+
+  let x = 10;
+  let y = x++;
+  console.log(x, y);
+
+
+
+  let a = 'red';
+  let b = 'blue';
+
+  let lista = [a,b];
+
+
+function swap(lista){
+  let c = lista[0];
+  lista[0] = lista[1];
+  lista[1] = c;
+//return(lista);
+}
+
+swap(lista);
+
+  console.log(lista[0]);
+  console.log(lista[1]);
---
+++ test.js
@@ -0,0 +1,54 @@
+
+let hour = 15;
+
+if (hour >= 6 && hour < 12) {
+    console.log('Good Morning');
+}
+else if (hour >= 12 && hour < 24) {
+    console.log('Good afternoon');
+}
+else if (hour >= 0 && hour < 6){
+    console.log('Good sleep');
+}
+else if (hour < 0 || hour > 24) {
+    console.log('This is not possible');
+} 
+else   
+ console.log('Good Evening');
+
+
+
+ //let role = 'guest';
+ let role = 'moderator';
+ //let role;
+
+ switch (role) {
+     case 'guest':
+        console.log('Guest User');
+        break;
+
+     case 'moderator':
+        console.log('Moderatore User');
+        break;
+
+    default:
+        console.log('Unknown user');
+ }
+
+ if (role=='guest')  console.log('guest user');
+ else if (role=='moderator') console.log('moderator user');
+ else console.log('unknown user');
+
+
+ for (let i = 1; i <= 5; i++) {
+    console.log('Hello world', i);
+    if (i % 2 !== 0) console.log(i);
+ }
+ 
+let i = 1;
+ while (i <=5) {
+    console.log('Hello world', i);
+    if (i % 2 !== 0) console.log(i);
+    i++
+}
+ 
