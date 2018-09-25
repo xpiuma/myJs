34e423b7a4616496bcde05d3de159f9b773d8ace
Davide <davide.piumetti@accenture.com>
2018-09-25 14:58:25 +0200

    object test

--- test3.js
+++ test3.js
@@ -1,8 +1,14 @@
-let circle = {
+const circle = {
     radius: 1,
     location: {
         x: 1,
         y: 1
     },
     isVisible: true,
+    draw: function() {
+        console.log('draw');
+    }
 };
+
+
+circle.draw();
